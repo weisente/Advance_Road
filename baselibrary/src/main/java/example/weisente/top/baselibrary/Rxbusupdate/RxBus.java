9fd952c742a4a4d87386ad1a495d9043102e6f37
1298267898@qq.com <1298267898@qq.com>
2017-09-30 16:22:34 +0800

    加入Rxbus   进行中

--- baselibrary/src/main/java/example/weisente/top/baselibrary/Rxbusupdate/RxBus.java
+++ baselibrary/src/main/java/example/weisente/top/baselibrary/Rxbusupdate/RxBus.java
@@ -6,8 +6,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.Subject;
 
 
 /**
@@ -30,6 +36,10 @@ public class RxBus {
     private Map<Object, List<Disposable>> disposablesMap = new HashMap<>();
     //存储所有粘性事件
     private final Map<Class<?>, StickyMessage> stickyEventMap = new ConcurrentHashMap<>();
+    //保存stickyEventMap的key
+    private final ConcurrentLinkedQueue<Class<?>> stickyEventKeys = new ConcurrentLinkedQueue<>();
+    // 主题，PublishSubject只会把在订阅发生的时间点之后来自原始Observable的数据发射给观察者
+    private final Subject<Object> bus = PublishSubject.create().toSerialized();
 
     /** 保存粘性事件的最大值 */
     private int MAX_STICKY_EVENT_COUNT = 10000;
@@ -73,11 +83,49 @@ public class RxBus {
 
                     addSubscriber(subscriber);
                     addSubscriberMethodInfoToMap(subscriber, subscriberMethodInfo);
+                    bindObservable(subscriberMethodInfo);
                 }
 
             }
         }
     }
+
+
+    /** 绑定observable */
+    private void bindObservable(final SubscriberMethodInfo subscriberMethodInfo){
+//        Disposable disposable = toObservable(Message.class, subscriberMethodInfo);
+    }
+
+    /** 根据传递的 eventType 类型返回特定类型(eventType)的 被观察者 */
+    private <T> Observable<T> toObservable(final Class<T> eventType, SubscriberMethodInfo methodInfo) {
+        //因为粘性和非粘性是分不同的两个容器去存储的  更加不同的StickyEvent 去判断
+
+        if (methodInfo.isReceiveStickyEvent()) {
+            synchronized (stickyEventMap) {
+                Observable<T> observable = bus.ofType(eventType).subscribeOn(Schedulers.io());
+                final StickyMessage stickyMessage = stickyEventMap.get(methodInfo.getEventType());
+
+                if (stickyMessage != null) {
+                    return observable.mergeWith(new Observable<T>() {
+                        @Override
+                        protected void subscribeActual(Observer<? super T> observer) {
+                            observer.onNext(eventType.cast(stickyMessage));
+                        }
+                    });
+                } else {
+                    return observable;
+                }
+            }
+
+        } else {
+            return bus.ofType(eventType).subscribeOn(Schedulers.io());
+        }
+    }
+
+
+
+
+
 
     /**
      * 将订阅者添加到map中，便于避免重复注册
