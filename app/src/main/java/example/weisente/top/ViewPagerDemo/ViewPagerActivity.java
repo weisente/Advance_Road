606d6fd86f56342783593828c1468b5d24880c55
weisente <1298267898@qq.com>
2017-10-29 22:21:04 +0800

    viewPage 不能滑动到最后一个最佳实现

---
+++ app/src/main/java/example/weisente/top/ViewPagerDemo/ViewPagerActivity.java
@@ -0,0 +1,29 @@
+package example.weisente.top.ViewPagerDemo;
+
+import example.weisente.top.baselibrary.base.BaseActivity;
+
+/**
+ * Created by san on 2017/10/29 0029.
+ */
+
+public class ViewPagerActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initTitle() {
+
+    }
+
+    @Override
+    protected void setContentView() {
+
+    }
+}
