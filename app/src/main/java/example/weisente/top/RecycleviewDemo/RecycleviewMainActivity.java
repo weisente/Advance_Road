cfa1caca985a7e33db623c500687e34c701af237
weisente <1298267898@qq.com>
2017-11-01 09:27:40 +0800

    打造recycleview各种框架

---
+++ app/src/main/java/example/weisente/top/RecycleviewDemo/RecycleviewMainActivity.java
@@ -0,0 +1,31 @@
+package example.weisente.top.RecycleviewDemo;
+
+import example.weisente.top.R;
+import example.weisente.top.baselibrary.base.BaseActivity;
+
+/**
+ * Created by san on 2017/11/1.
+ */
+
+public class RecycleviewMainActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initTitle() {
+
+    }
+
+    @Override
+    protected void setContentView() {
+        //设置recycleview的视图
+        setContentView(R.layout.activity_recycleview);
+    }
+}
---
+++ app/src/main/java/example/weisente/top/ScreenFixDemo/ScreenFixActivity.java
@@ -0,0 +1,29 @@
+package example.weisente.top.ScreenFixDemo;
+
+import example.weisente.top.baselibrary.base.BaseActivity;
+
+/**
+ * Created by san on 2017/10/31.
+ */
+
+public class ScreenFixActivity  extends BaseActivity{
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initTitle() {
+
+    }
+
+    @Override
+    protected void setContentView() {
+
+    }
+}
