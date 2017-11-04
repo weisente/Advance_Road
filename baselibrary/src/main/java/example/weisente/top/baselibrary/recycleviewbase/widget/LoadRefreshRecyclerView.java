4394391c8a43f156b1650f6e50654b987fea223a
weisente <1298267898@qq.com>
2017-11-04 16:51:07 +0800

    添加上啦加载更多！！！！

---
+++ baselibrary/src/main/java/example/weisente/top/baselibrary/recycleviewbase/widget/LoadRefreshRecyclerView.java
@@ -0,0 +1,59 @@
+package example.weisente.top.baselibrary.recycleviewbase.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by san on 2017/11/4.
+ */
+
+public class LoadRefreshRecyclerView extends RefreshRecyclerView {
+    // 上拉加载更多的辅助类
+    private LoadViewCreator mLoadCreator;
+    // 上拉加载更多头部的高度
+    private int mLoadViewHeight = 0;
+    // 上拉加载更多的头部View
+    private View mLoadView;
+    // 手指按下的Y位置
+    private int mFingerDownY;
+    // 当前是否正在拖动
+    private boolean mCurrentDrag = false;
+    // 当前的状态
+    private int mCurrentLoadStatus;
+    // 默认状态
+    public int LOAD_STATUS_NORMAL = 0x0011;
+    // 上拉加载更多状态
+    public static int LOAD_STATUS_PULL_DOWN_REFRESH = 0x0022;
+    // 松开加载更多状态
+    public static int LOAD_STATUS_LOOSEN_LOADING = 0x0033;
+    // 正在加载更多状态
+    public int LOAD_STATUS_LOADING = 0x0044;
+    public LoadRefreshRecyclerView(Context context) {
+        super(context);
+    }
+
+    public LoadRefreshRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoadRefreshRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+
+
+    // 处理加载更多回调监听
+    private OnLoadMoreListener mListener;
+
+    public void setOnLoadMoreListener(OnLoadMoreListener listener) {
+        this.mListener = listener;
+    }
+
+    public interface OnLoadMoreListener {
+        void onLoad();
+    }
+
+}
