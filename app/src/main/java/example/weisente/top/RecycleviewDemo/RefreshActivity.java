150e13c34c5a42361ea37b7f141b089dff6c73fc
weisente <1298267898@qq.com>
2017-11-04 16:44:57 +0800

    顺利添加下拉刷新

--- app/src/main/java/example/weisente/top/RecycleviewDemo/RefreshActivity.java
+++ app/src/main/java/example/weisente/top/RecycleviewDemo/RefreshActivity.java
@@ -1,46 +1,46 @@
 package example.weisente.top.RecycleviewDemo;
 
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import example.weisente.top.R;
 import example.weisente.top.baselibrary.base.BaseActivity;
-import example.weisente.top.baselibrary.recycleviewbase.widget.WrapRecyclerView;
+import example.weisente.top.baselibrary.recycleviewbase.widget.RefreshRecyclerView;
 
 /**
  * Created by san on 2017/11/4.
  */
 
-public class HeadFootActivity  extends BaseActivity {
-    WrapRecyclerView rv;
+public class RefreshActivity extends BaseActivity {
+    RefreshRecyclerView rv;
     private List<String> mDatas;
     private RecycleviewMainActivity.HomeAdapter mAdapter;
-
     @Override
     protected void initData() {
-        rv  = (WrapRecyclerView) findViewById(R.id.rv);
+        rv = (RefreshRecyclerView) findViewById(R.id.rv);
         rv.setLayoutManager(new LinearLayoutManager(this));
         mDatas = new ArrayList<>();
         for (int i = 'A'; i < 'z'; i++) {
             mDatas.add("" + (char) i);
         }
+
         mAdapter = new RecycleviewMainActivity.HomeAdapter(this, mDatas);
         rv.setAdapter(mAdapter);
-//        TextView textView = new TextView(this);
-//        textView.setText("头部");
-        View inflate = LayoutInflater.from(this).inflate(R.layout.headview, null);
-        rv.addHeaderView(inflate);
-
-        TextView foot = new TextView(this);
-        foot.setText("尾部");
-        rv.addFooterView(foot);
-
-
+        rv.addRefreshViewCreator(new DefaultRefreshCreator());
+        rv.setOnRefreshListener(new RefreshRecyclerView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        rv.onStopRefresh();
+                    }
+                }, 2000);
+            }
+        });
     }
 
     @Override
@@ -55,6 +55,6 @@ public class HeadFootActivity  extends BaseActivity {
 
     @Override
     protected void setContentView() {
-        setContentView(R.layout.activity_foothead);
+        setContentView(R.layout.activity_refresh);
     }
 }
