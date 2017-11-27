daf2b1de76942a86ca027a42d1214440d15964d4
weisente <1298267898@qq.com>
2017-11-27 20:56:12 +0800

    Apt   module建成！！！！！！

--- baselibrary/src/main/java/example/weisente/top/baselibrary/AOPCheckNet/SectionAspect.java
+++ baselibrary/src/main/java/example/weisente/top/baselibrary/AOPCheckNet/SectionAspect.java
@@ -1,11 +1,17 @@
 package example.weisente.top.baselibrary.AOPCheckNet;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 
 /**
  * Created by san on 2017/11/27.
@@ -17,17 +23,53 @@ public class SectionAspect {
      * 找到处理的切点
      * * *(..)  可以处理所有的方法
      */
-    @Pointcut("example.weisente.top.baselibrary.AOPCheckNet.CheckNet * *(..))")
+    @Pointcut("execution(@example.weisente.top.baselibrary.AOPCheckNet.CheckNet * *(..))")
     public void checkNetBehavior() {
-        Log.e("checkNetBehavior","这是一个点");
+
     }
 
     /**
      * 处理切面
      */
     @Around("checkNetBehavior()")
-    public Object checkNet(ProceedingJoinPoint joinPoint) throws Throwable{
-        Log.e("checkNet","这是一个面");
+    public Object checkNet(ProceedingJoinPoint joinPoint) throws Throwable {
+        Log.e("TAG", "checkNet");
+        // 做埋点  日志上传  权限检测（我写的，RxPermission , easyPermission） 网络检测
+        // 网络检测
+        // 1.获取 CheckNet 注解  NDK  图片压缩  C++ 调用Java 方法
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        CheckNet checkNet = signature.getMethod().getAnnotation(CheckNet.class);
+        if (checkNet != null) {
+            // 2.判断有没有网络  怎么样获取 context?
+            Object object = joinPoint.getThis();// View Activity Fragment ； getThis() 当前切点方法所在的类
+            Context context = getContext(object);
+            if (context != null) {
+                if (!CheckNetUtil.isNetworkAvailable(context)) {
+                    // 3.没有网络不要往下执行
+                    Toast.makeText(context,"请检查您的网络",Toast.LENGTH_LONG).show();
+                    return null;
+                }
+            }
+        }
         return joinPoint.proceed();
+    }
+
+    /**
+     * 通过对象获取上下文
+     *
+     * @param object
+     * @return
+     */
+    private Context getContext(Object object) {
+        if (object instanceof Activity) {
+            return (Activity) object;
+        } else if (object instanceof Fragment) {
+            Fragment fragment = (Fragment) object;
+            return fragment.getActivity();
+        } else if (object instanceof View) {
+            View view = (View) object;
+            return view.getContext();
+        }
+        return null;
     }
 }
